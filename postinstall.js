88629726cfa14f471863f4f188aa77f04df97c00
Darren Reid <darren@reidmail.org>
2021-10-18 15:53:11 +1100

    Try to remove all use of trailing `.html`.

---
+++ postinstall.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const glob = require('glob');
+let js = 'node_modules/vitepress/dist/client/app/router.js';
+fs.writeFileSync(js, fs.readFileSync(js, 'utf8').replace("url.pathname += '.html';", ''))
+
+glob('node_modules/vitepress/dist/node/serve-*.js',{},(err,files) =>{
+    let file = files[0];
+    fs.writeFileSync(file,fs.readFileSync(file,'utf8').replace("cleanUrl += \".html\";",'//cleanUrl += ".html";'))
+})
