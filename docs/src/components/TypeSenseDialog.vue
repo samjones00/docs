eafcdb35de0aa73fe81a45ea8aedd65a28957687
Darren Reid <darren@reidmail.org>
2021-10-22 11:06:51 +1100

    Tighten up search dialog.

--- docs/src/components/TypeSenseDialog.vue
+++ docs/src/components/TypeSenseDialog.vue
@@ -153,7 +153,7 @@ function stripHtml(s:string) {
   <div class="search-dialog hidden flex bg-black bg-opacity-25 items-center" :class="{ open }" 
        @click="$emit('hide')">
     <div class="dialog absolute" style="max-height:70vh;" @click.stop="false">
-      <div class="p-2 flex flex-col">
+      <div class="p-2 flex flex-col" style="max-height: 70vh;">
         <div class="flex">
           <label class="pt-4 mt-0.5 pl-2" for="docsearch-input">
             <svg class="w-8 h-8 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
